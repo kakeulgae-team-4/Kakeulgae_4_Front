5248e479a54ee7dfc8166cebf06077b332224761
ChoiHyebin <bini0819@catholic.ac.kr>
2024-03-18 14:35:06 +0900

    로그인 여부에 따른 메인 화면 변화(로컬테스트용으로 임시 구현)

--- src/App.js
+++ src/App.js
@@ -8,19 +8,22 @@ import Signin from './web/routes/Signin';
 import Signup from './web/routes/Signup';
 import Mypage from './web/routes/Mypage';
 import Allrecruit from './web/routes/Allrecruit';
+import { AuthProvider } from './web/AuthProvider';
 
 const App = () => {
     return (
         <BrowserRouter>
             <div className="App">
-                <Routes>
-                    <Route path='/' element={<Main />} />
-                    <Route path='/signin' element={<Signin />} />
-                    <Route path='/signup' element={<Signup />} />
-                    <Route path='/mypage' element={<Mypage />}/>
-                    <Route path='/allrecruit' element={<Allrecruit />}/>
-                </Routes>
-                <Footer />
+                <AuthProvider>
+                    <Routes>
+                        <Route path='/' element={<Main />} />
+                        <Route path='/signin' element={<Signin />} />
+                        <Route path='/signup' element={<Signup />} />
+                        <Route path='/mypage' element={<Mypage />}/>
+                        <Route path='/allrecruit' element={<Allrecruit />}/>
+                    </Routes>
+                    <Footer />
+                </AuthProvider>
             </div>
         </BrowserRouter>
     )
