2d59d5324e2d17a0120a2eef7298b330ea758a2a
Chae <oumuamua121@gmail.com>
2024-03-19 09:51:32 +0900

    로그인 구현 후 로그인에 따른 메인페이지 전환

--- src/web/components/RegisterForm.js
+++ src/web/components/RegisterForm.js
@@ -7,14 +7,13 @@ const RegisterForm =  ({ setRegisterFormOpen }) => {
   
   const handleSubmit = async (event) => { // form을 submit할 때 실행되는 함수
     event.preventDefault(); // 기본 이벤트를 막음
-    console.log(`nickname :${event.target.nickname.value}`);
-    const res =  await fetch("/api/v1/auth/register/google", {
+    const res =  await fetch("http://localhost:8080/api/v1/auth/register/google", {
       method: "POST",
       headers: defaultHeaders,
       body: JSON.stringify({
         idToken: event.target.idToken.value,
         nickname: event.target.nickname.value,
-
+        email: event.target.email.value,
       }),
     });
     const user = await res.json();
@@ -26,11 +25,15 @@ const RegisterForm =  ({ setRegisterFormOpen }) => {
   return (
     <div>
       <form onSubmit={handleSubmit}>
+        <input type="hidden" name="idToken" value={localStorage.getItem("idToken")} />
         <label className='nickname'>
              Enter your nickname
         </label>
         <input className='nickname' type="text" name="nickname" />
-        
+        <label className='email'>
+             Enter your email
+        </label>
+        <input className='email' type="email" name="email" />
         <input className='signup' type="submit" value="Sign up" />
       </form>
     </div>
