bf77d3bdc09fd84ff1fcad2665a3cbe9e05df884
ChoiHyebin <bini0819@catholic.ac.kr>
2024-03-25 17:31:13 +0900

    import React

--- src/web/components/Profile.js
+++ src/web/components/Profile.js
@@ -1,4 +1,5 @@
-import React, {useContext} from 'react'
+import React from 'react';
+import {useContext} from 'react'
 import './Profile.css';
 import profile from '../images/cute.jpg';
 import { UserContext } from '../components/AuthProvider';
