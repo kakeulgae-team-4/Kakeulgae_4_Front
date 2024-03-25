aa28ccb3027404a0bfc56a22a146cc8c19403a02
ChoiHyebin <bini0819@catholic.ac.kr>
2024-03-25 17:54:27 +0900

    토큰 수정

--- src/web/routes/Bookmark.js
+++ src/web/routes/Bookmark.js
@@ -11,7 +11,8 @@ const Bookmark = () => {
     const [bookmarkList, setBookmarkList] = useState([]);
     const [userList, setUserList] = useState([]);
     const [showGallery, setShowGallery] = useState(true);
-    const accessToken = '토큰';
+    const accessToken = localStorage.getItem('idToken');
+    console.log(accessToken);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -20,7 +21,7 @@ const Bookmark = () => {
                     {
                         headers: {
                           Authorization: `Bearer ${accessToken}`
-                        },                                                                                                                                                            
+                        },                                                                                     
                     }
                 );
                 const user = await axios.get('http://localhost:8080/api/v1/member/info',
